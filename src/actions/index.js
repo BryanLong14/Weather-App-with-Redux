edc719856ae062ee9c033b07c6d36494765ad966
Bryan Long <BryanLong1@gmail.com>
2018-05-16 12:14:15 -0600

    Added Middleware for Reducers in Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,8 +9,11 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
+  console.log("Request", request);
+
   return {
     type: FETCH_WEATHER,
+    // return the promise from axios as the payload below
     payload: request
   };
 }
